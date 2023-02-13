ba82eb6bee67ddb695a76dddd9b27da957659a5d
Ephraim Machtinger <ephraim.machtinger@c3.ai>
2023-02-12 21:49:20 -0800

    Testing the CI/CD pipeline

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel, conlist
 
 
-
+# Adding comment to run unit testing
 app = FastAPI(title="Predicting Wine Class with batching")
 
 # Open classifier in global scope
